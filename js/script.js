a9056277062acc8b2297b8b1200a1c77d4777a4c
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2021-12-17 15:24:05 +0100

    addinf bonuses and third milestone

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const app = new Vue(
   {
     el: '#app',
     data: {
+      todoNew: '',
       todos: [
         {
           text: 'prepare la pasta',
@@ -22,9 +23,21 @@ const app = new Vue(
           todo.done = false;
         }
       },
-      removeTodo: function (todo) {
-        this.todos.splice(todo, 1); 
-      }
+      removeTodo: function (todo, index) {
+        this.todos.splice(todo, index); 
+        console.log(todo, index);
+      },
+      addTodo: function () {
+        if (this.todoNew.length != 0) {
+          let obj = {
+            text: this.todoNew,
+            done: ''
+          }
+          this.todos.push(obj); 
+        }  
+      },
     }
   }
 )
+
+// Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
